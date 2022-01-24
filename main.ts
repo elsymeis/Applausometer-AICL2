86d506d5e2622c90be662fbf0818a82cc6599409
elsymeis <56837582+elsymeis@users.noreply.github.com>
2022-01-24 12:22:37 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showIcon(IconNames.Yes)
+let strip = neopixel.create(DigitalPin.P12, 5, NeoPixelMode.RGB)
+strip.setBrightness(50)
+basic.forever(function () {
+    if (gatorMicrophone.getSoundIntensity() > 1000) {
+        basic.clearScreen()
+        strip.showBarGraph(gatorMicrophone.getSoundIntensity(), 2000)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
